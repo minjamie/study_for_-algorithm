58330aec66fba41bd32413308bf02310464bbd60
minjaekim <minjae2246@gmail.com>
2023-01-06 21:38:26 +0900

    배열과 오브젝트의 성능 평가 학습

---
+++ 01/03_Performance_Evaluation_Of_Arrays_Objects.js
@@ -0,0 +1,12 @@
+let instructor = {
+    firstName: 'Minjae',
+    isInstructor: true,
+    favoriteNums: [1, 2, 3, 4]
+}
+
+Object.keys(instructor);
+Object.values(instructor);
+
+let names = ['a', 'b', 'c ']
+
+names.push('d')
